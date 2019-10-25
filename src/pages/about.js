a50c0a8d63e3357e6e48b3350b5b7d4fddf96efd
Brian Rogers <rogers.brian@gmail.com>
2019-10-25 13:34:29 -0700

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const About = () => (
   <Layout>
     <SEO title="About" />
     <h1>About Us</h1>
-    <p>4252 Concepts was founded by Brian Rogers with the intention to be creative, helpful, and inspiring. Primarily through community involvement, the company is focused on helping small businesses and individuals with various services which include technology, photography, graphic arts, video creation, and, most importantly, achieving their potential.</p>
+    <p>4252 Concepts was founded by Brian Rogers with the intention to be creative, helpful, and inspiring. Primarily through community involvement, the company is focused on helping individuals to large businesses with various services which include technology, photography, graphic arts, video creation, and, most importantly, achieving their potential.</p>
     <p>Over the years, 4252 Concepts has provided thought leadership, strategy assessment, and execution improvement processes. Clients ranging from medical, legal, transportation, fashion, and more have benefited from the diverse perspectives provided and have seen growth after realigning their vision strategy with a clear set of goals and guiding principals.</p>
   </Layout>
 )
