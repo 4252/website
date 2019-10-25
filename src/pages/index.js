bc5ed25ac2eb5caa2d9cf06ff1eb1c7d434ebb5d
Brian Rogers <rogers.brian@gmail.com>
2019-10-25 14:43:17 -0700

    Add home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,15 @@ const IndexPage = (props) => (
     <SEO title="Home" />
     <Container fluid style={{ paddingRight: 0, paddingLeft: 0 }}>
       <Row noGutters>
-        <Col xs={{ span: 12, order: 2 }} sm={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} >sm=8</Col>
-        <Col xs={{ span: 12, order: 1 }} sm={{ span: 12, order: 1 }} md={{ span: 7, order: 2 }} ><Img fluid={props.data.homeImg.childImageSharp.fluid} /></Col>
+        <Col xs={{ span: 12, order: 2 }} sm={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} style={{ display: "flex", fontSize: "30px" }}>
+          <p style={{ margin: "auto", textAlign: "center" }}>
+          <span style={{ fontSize: "30px" }}>Advanced ideas for the most complex home office and business needs.</span>
+          <br /><br />
+          <span style={{ fontSize: "20px", color: "darkgray" }}>Book time with us: <a href="https://x52.fyi/bookings" target="_blank" rel="noopener noreferrer">bookings page</a></span></p>
+        </Col>
+        <Col xs={{ span: 12, order: 1 }} sm={{ span: 12, order: 1 }} md={{ span: 7, order: 2 }} >
+          <Img fluid={props.data.homeImg.childImageSharp.fluid}/>
+        </Col>
       </Row>
     </Container>
   </Layout>
