8e1547022ce9358dca24777d5cf7b910a1ac0af5
Brian Rogers <rogers.brian@gmail.com>
2019-10-25 13:34:01 -0700

    Update home page with image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import { Container, Col, Row } from 'react-bootstrap'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Container fluid style={{ paddingRight: 0, paddingLeft: 0 }}>
+      <Row noGutters>
+        <Col xs={{ span: 12, order: 2 }} sm={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} >sm=8</Col>
+        <Col xs={{ span: 12, order: 1 }} sm={{ span: 12, order: 1 }} md={{ span: 7, order: 2 }} ><Img fluid={props.data.homeImg.childImageSharp.fluid} /></Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
 export default IndexPage
+
+export const fluidImage = graphql`
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 1920) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+`
+
+export const pageQuery = graphql`
+  query {
+    homeImg: file(relativePath: { eq: "home_splash.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
