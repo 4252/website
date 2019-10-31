805330fbc8a443ac7b1e2fe2a8512874f9664d93
Brian Rogers <i4252@outlook.com>
2019-10-31 11:55:59 -0700

    Change home screen message

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = (props) => (
       <Row noGutters>
         <Col xs={{ span: 12, order: 2 }} sm={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} style={{ display: "flex", fontSize: "30px" }}>
           <p style={{ margin: "auto", textAlign: "center" }}>
-          <span style={{ fontSize: "30px" }}>Advanced ideas for the most complex home office and business needs.</span>
+          <span style={{ fontSize: "30px" }}>Effective solutions for the most complex home office and business needs.</span>
           <br /><br />
           <span style={{ fontSize: "20px", color: "darkgray" }}>Book time with us: <a href="https://x52.fyi/bookings" target="_blank" rel="noopener noreferrer">bookings page</a></span></p>
         </Col>
