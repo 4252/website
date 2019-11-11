012ef35492b89afe56cd7fe44f54940dc2697f88
Brian Rogers <i4252@outlook.com>
2019-11-11 14:33:05 -0800

    Reorder content on services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -8,6 +8,27 @@ const Services = () => (
     <Content banner="Services">
         <SEO title="Services" />
         <div>
+            <div className="servicesHeading">Software and Cloud</div>
+            Most people use email as a primary source of communication. Even with text messaging, Slack, Microsoft Teams, and many other types of services that keep people together, email remains the default. Getting a personal email address is simple and free! Why wouldn't you want to get something for free?
+            <br/>
+            <br/>
+            What many people don't know is that, if your email account gets locked out, they don't have to return access to you. Although many of the large, free services have taken measures to let you get your login information back, this doesn't mean it is a guarantee.
+            <br/>
+            <br/>
+            One of our biggest complaints here at 4252 Concepts is that these free services don't provide you a vanity domain name. Have you ever wanted a remarkable email address, like frank@rocks.com or something similar? To do that, you need to buy into a paid service. Many of these services significantly reduce your security and can leave you without some of the standard features found in the free services like calendar and contact synchronization.
+            <br/>
+            <br/>
+            Contact us today to help you get your own Office 365 account with a custom domain name. Let us configure your security settings and ensure you have what you need to streamline your communication.
+            <br/>
+            <br/>
+            <ul>
+                <li>Get your own domain name and custom email address!</li>
+                <li>Increase your security!</li>
+                <li>Work with partner companies and vendors as if you were enterprise size yourself</li>
+                <li>Get free conferencing abilities; buy up and get your dial-in number</li>
+            </ul>
+        </div>
+        <div>
             <div className="servicesHeading">Networking</div>
             Today, the need for Wi-Fi and cellular communications is continuously increasing. Let us help you install your new Wi-Fi solution in your home or office. Although there are many choices out there, we offer professional-grade solutions at consumer prices.
             <br/>
@@ -34,28 +55,6 @@ const Services = () => (
             <br/>
             We have worked with apartment buildings and homes to install cellular signal boosters. These devices help you
             make the calls you want, while supporting anyone else who visits your home or office as well.
-        </div>
-
-        <div>
-            <div className="servicesHeading">Software and Cloud</div>
-            Most people use email as a primary source of communication. Even with text messaging, Slack, Microsoft Teams, and many other types of services that keep people together, email remains the default. Getting a personal email address is simple and free! Why wouldn't you want to get something for free?
-            <br/>
-            <br/>
-            What many people don't know is that, if your email account gets locked out, they don't have to return access to you. Although many of the large, free services have taken measures to let you get your login information back, this doesn't mean it is a guarantee.
-            <br/>
-            <br/>
-            One of our biggest complaints here at 4252 Concepts is that these free services don't provide you a vanity domain name. Have you ever wanted a remarkable email address, like frank@rocks.com or something similar? To do that, you need to buy into a paid service. Many of these services significantly reduce your security and can leave you without some of the standard features found in the free services like calendar and contact synchronization.
-            <br/>
-            <br/>
-            Contact us today to help you get your own Office 365 account with a custom domain name. Let us configure your security settings and ensure you have what you need to streamline your communication.
-            <br/>
-            <br/>
-            <ul>
-                <li>Get your own domain name and custom email address!</li>
-                <li>Increase your security!</li>
-                <li>Work with partner companies and vendors as if you were enterprise size yourself</li>
-                <li>Get free conferencing abilities; buy up and get your dial-in number</li>
-            </ul>
         </div>
     </Content>
 )
