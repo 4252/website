c413fdf5e70172388b66e6c5b2b90650953f46df
Brian Rogers <i4252@outlook.com>
2019-11-11 16:15:31 -0800

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: `4252 Concepts`,
-    description: `4252 Concepts was founded with the intention to be creative, helpful, and inspiring. \
-                  Primarily through community involvement, the company is focused on helping individuals to large \
-                  businesses with various services which include technology, photography, graphic arts, video creation, \
-                  and, most importantly, achieving their potential. Areas: Microsoft, Azure, Office, Office 365, Outlook, \
-                  SharePoint, Teams, Amazon Web Services (AWS), Alexa, Networking, TCP, IP, VPN, DNS, Router, WiFi, \
-                  Wireless, Cellular, Installation, Integration, SSL, TLS, Security, Active Directory, RADIUS Authentication, WPA2, WPA \
-                  Container, Docker, Kubernetes, React, ReactJS, NodeJS, Development, Visual Studio, Visual Code, Debugging \
-                  Wireshark, Backup, Disaster Recovery, Architecture, Information Technology, IT, Support, Desktop, \
-                  Windows, Windows 10, Linux, Ubuntu, Lambda, Azure Functions, Virtual Private Networking, Domain Name Server, \
-                  Ubiquiti, Unifi, Video Surveillance, Photography, Washington, WA, Bellevue, Redmond, Kirkland, Cle Elum, \
-                  Issaquah, Kent, Renton, Seattle, Global, National, International, Consulting, Management, Services, Solutions \
-                  Small Business, SMB`,
+    description: `4252 Concepts was founded with the intention to be creative, helpful, and inspiring. ` +
+                  `Primarily through community involvement, the company is focused on helping individuals to large ` +
+                  `businesses with various services which include technology, photography, graphic arts, video creation, ` +
+                  `and, most importantly, achieving their potential. Areas: Microsoft, Azure, Office, Office 365, Outlook, ` +
+                  `SharePoint, Teams, Amazon Web Services (AWS), Alexa, Networking, TCP, IP, VPN, DNS, Router, WiFi, ` +
+                  `Wireless, Cellular, Installation, Integration, SSL, TLS, Security, Active Directory, RADIUS Authentication, WPA2, WPA ` +
+                  `Container, Docker, Kubernetes, React, ReactJS, NodeJS, Development, Visual Studio, Visual Code, Debugging ` +
+                  `Wireshark, Backup, Disaster Recovery, Architecture, Information Technology, IT, Support, Desktop, ` +
+                  `Windows, Windows 10, Linux, Ubuntu, Lambda, Azure Functions, Virtual Private Networking, Domain Name Server, ` +
+                  `Ubiquiti, Unifi, Video Surveillance, Photography, Washington, WA, Bellevue, Redmond, Kirkland, Cle Elum, ` +
+                  `Issaquah, Kent, Renton, Seattle, Global, National, International, Consulting, Management, Services, Solutions ` +
+                  `Small Business, SMB`,
     twitterDescription: `4252 Concepts was founded with the intention to be creative, helpful, and inspiring. \
                   Primarily through community involvement, the company is focused on helping individuals to large \
                   businesses with various services which include technology, photography, graphic arts, video creation, \
@@ -41,6 +41,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/logo_64px.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-7725473-8",
+        head: false
       },
     },
   ],
