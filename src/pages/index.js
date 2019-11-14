d08cccd496ba85f99379807e894e6efc4070c3ce
Brian Rogers <i4252@outlook.com>
2019-11-14 08:39:07 -0800

    Format home page for view height improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import SEO from '../components/seo'
 const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
-    <Container fluid style={{ paddingRight: 0, paddingLeft: 0, backgroundColor: "white", height: "96vh", display: "flex", flexFlow: "column" }}>
+    <Container fluid style={{ paddingRight: 0, paddingLeft: 0, backgroundColor: "white", height: "92vh", display: "flex", flexFlow: "column" }}>
       <Row noGutters style={{ flexGrow: "1" }}>
         <Col xs={{ span: 12, order: 2 }} sm={{ span: 12, order: 2 }} md={{ span: 5, order: 1 }} style={{ display: "block", margin: "auto", textAlign: "center" }}>
           <h2>Effective technology solutions for the most complex office needs. What concept can we help you with today?</h2>
